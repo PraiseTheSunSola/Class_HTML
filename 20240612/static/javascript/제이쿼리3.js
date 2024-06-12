5529df9287d0c38597ea37fc9e8a4c845f50dc67
kang1211 <zdxsca1211@naver.com>
2024-06-12 16:24:20 +0900

    실습

---
+++ 20240612/static/javascript/제이쿼리3.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#name").hide();
+    $("#age").hide();
+});
+
+/*
+    이름 입력 버튼과 나이 입력 버튼을 각각 클릭하여 input 나타내어 이름과 나이 입력하세요.
+    등록 버튼을 클릭하여 화면에 이름, 나이, 태어난 년도를 출력하세요.
+*/
+
+/*
+    <button id="nameInput">이름 입력</button> <input type="text" id="name"> <br>
+    <button id="nameInput">나이 입력</button> <input type="text" id="age"> <br>
+    <hr>
+    <div>
+        이름 : <b id="nameView"></b> <br>
+        나이 : <b id="ageView"></b> <br>
+        태어난 년도 : <b id="yearView"></b> <br>
+    </div>
+*/
+
