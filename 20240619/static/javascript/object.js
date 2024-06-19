bff57081302d0df20e52b19dfbba18f4ece4c716
kang1211 <zdxsca1211@naver.com>
2024-06-19 14:12:54 +0900

    생성자 함수 및 객체를 배열에 저장한다.

--- 20240619/static/javascript/object.js
+++ 20240619/static/javascript/object.js
@@ -9,3 +9,42 @@ for(var i=0; i< arr.length; i++){
 }
 */
 
+// 객체 - 직접 손으로 만질 수 있는 물건.
+
+/* 프로그램에서의 객체
+변수, 10, "이순신" 등 모양과 행동을 가지고 있는 대상.
+데이터와 함수를 가질 수 있는 대상.
+
+ex) 모니터 객체 
+    함수 : 출력, 전원버튼, 메뉴 => 색, 비율, 초기화, 음량
+    데이터: 모델명, 제조사 등...
+
+    배열 a = []; , b = new Array();
+    데이터 - > 배열의 공간에 저장 되어 있는 값
+          -> 배열의 크기
+    기능 -> push(), splice(), indexOf()...
+
+    html 태그는 자바스크립트에서 객체이다.
+*/
+
+// window.onload = function(){
+
+//     let d = document.getElementById("box");
+//     console.log(d);
+
+//     document.write(d.innerHTML); 
+//     // 객체에서 .은 접근연산자이다.
+//     //객체 내부의 변수 또는 함수에 접근한다.
+
+//     document.write(d.id);
+//     document.write(d.className);
+//     document.write(d.localName);
+//     d.className = "box1";
+//     d.hidden = true;
+    
+//     let div = document.querySelector("#box");
+//     console.log(div);
+
+//     let box = $("#box");
+//     console.log(box);
+// }
